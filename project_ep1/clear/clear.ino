d7f4441ddd4408065feb08d537a0f3c7da531d91
samat <75171040+samat2002@users.noreply.github.com>
2023-11-16 14:04:58 +0700

    Add files via upload

---
+++ project_ep1/clear/clear.ino
@@ -0,0 +1,25 @@
+/*
+   EEPROM Clear
+
+   Sets all of the bytes of the EEPROM to 0.
+   This example code is in the public domain.
+
+*/
+
+#include <EEPROM.h>
+int val = 4096; // write the size of EEPROM of your board in bytes (from 0 to all 4096 bytes of the EEPROM)
+// if Arduino UNO then replace it with 512
+void setup() {
+  Serial.begin(9600);
+  EEPROM.begin(val); 
+  for (int i = 0; i < val; i++) {
+    EEPROM.write(i, 0);
+    Serial.println(i);
+  }
+
+  EEPROM.end();
+  Serial.println("EEPROM Clear");
+}
+
+void loop() {
+}
